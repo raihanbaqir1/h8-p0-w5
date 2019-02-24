94e9386afd34406ea8b9329517bac31f44c9c585
Raihan <raihanbaqir@gmail.com>
2019-02-24 20:36:39 +0700

    trying to apply regex, failed horribly

--- exercises9.js
+++ exercises9.js
@@ -1,24 +1,5 @@
 function cariPelaku(str) {
-    if(!str.length){
-        return 0
-    } else{
-        var key = "abc"
-        var test = str.substring(0,3)
-        if(test === key){
-            return cariPelaku(str.substr(1))+1
-        } else{
-            return cariPelaku(str.substr(1))
-        }
-
-    }
-    // console.log(test)
-    // var counter = 0
-    // for(var i = 0; i < str.length; i++){
-    //     if(str[i]+str[i+1]+str[i+2] === key){
-    //         counter++
-    //     }
-    // }
-    // return counter
+    console.log(str.match(/abc/))
 
   }
   
